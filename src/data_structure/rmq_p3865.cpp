04cac30fcc5ba812d06d2f49d16fc64de77691d3
shellspicker <shellspicker@outlook.com>
2020-03-05 18:59:00 +0800

    modify rmq
    use function constructor.
    
    Signed-off-by: shellspicker <shellspicker@outlook.com>

--- src/data_structure/rmq_p3865.cpp
+++ src/data_structure/rmq_p3865.cpp
@@ -443,31 +443,27 @@ public:
 #ifndef RMQ_H
 #define RMQ_H 1
 
-template<typename tp, bool is_min>
+template<typename tp, class func = function<tp(tp, tp)>>
 class rmq {
-	vector<tp> ds[20];
+	vector<vector<tp>> ds;
 public:
+	func fn;
+	rmq(func f) : fn(f) {}
 	void init(const vector<tp> &v) {
 		int sz = v.size();
+		ds.resize(ctz(roundup_pow_of_2(sz)));
 		for (auto &dim : ds)
 			dim.resize(sz);
 		fup (i, 0, sz - 1)
 			ds[0][i] = v[i];
 		for (int b = 1; (1 << b) <= sz; ++b)
-			for (int s = 0; s + (1 << b) <= sz; ++s) {
-				if (is_min)
-					ds[b][s] = min(ds[b - 1][s], ds[b - 1][s + (1 << b - 1)]);
-				else
-					ds[b][s] = max(ds[b - 1][s], ds[b - 1][s + (1 << b - 1)]);
-			}
+			for (int s = 0; s + (1 << b) <= sz; ++s)
+				ds[b][s] = fn(ds[b - 1][s], ds[b - 1][s + (1 << b - 1)]);
 	}
 	tp query(int l, int r) {
 		range_normalize(l, r);
 		tp xp = ctz(rounddown_pow_of_2(length(l, r)));
-		if (is_min)
-			return min(ds[xp][l], ds[xp][rtol(r, 1 << xp)]);
-		else
-			return max(ds[xp][l], ds[xp][rtol(r, 1 << xp)]);
+		return fn(ds[xp][l], ds[xp][rtol(r, 1 << xp)]);
 	}
 };
 
@@ -491,7 +487,7 @@ public:
 class data {
 	vector<int> v;
 	vector<query> qa;
-	rmq<int, 0> dsm{};
+	rmq<int> dsm{[](int a, int b) { return max(a, b); }};
 	istream &ioend() {
 		cin.setstate(ios_base::badbit);
 		return cin;
