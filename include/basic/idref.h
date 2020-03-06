1be2e3d39e19bb41da9a975b386c4e53f916d516
shellspicker <shellspicker@outlook.com>
2020-03-06 19:31:46 +0800

    add idref
    Signed-off-by: shellspicker <shellspicker@outlook.com>

---
+++ include/basic/idref.h
@@ -0,0 +1,36 @@
+#ifndef IDREF_H
+#define IDREF_H 1
+
+/*
+ * 伪离散化.
+ */
+template <typename T>
+class idref {
+	int id_cnt;
+	map<T, int> mmp;
+	map<int, T> orig;
+public:
+	void init() {
+		id_cnt = 0;
+		mmp.clear();
+		orig.clear();
+	}
+	int cnt() {
+		return id_cnt;
+	}
+	int get_id(T s)
+	{
+		if (mmp.count(s))
+			return mmp[s];
+		int res;
+		mmp[s] = res = id_cnt++;
+		orig[res] = s;
+		return res;
+	}
+	T get_orig(int id) {
+		assert(orig.count(id));
+		return orig[id];
+	}
+};
+
+#endif
