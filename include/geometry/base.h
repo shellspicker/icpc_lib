9e4506cda9ce5a640fbf2d2d4fb015598406623f
shellspicker <shellspicker@outlook.com>
2020-12-24 10:07:01 +0800

    update include/geometry/base.h
    Signed-off-by: shellspicker <shellspicker@outlook.com>

--- include/geometry/base.h
+++ include/geometry/base.h
@@ -57,7 +57,7 @@ struct pov {
 		return x * r.y - y * r.x;
 	}
 	double angle(const pov &r) {
-		acos(dot(r) / len() / r.len());
+		return acos(dot(r) / len() / r.len());
 	}
 	double area2(const pov &b, const pov &c) {
 		return (b - *this).cross(c - *this);
@@ -71,35 +71,54 @@ struct pov {
 	}
 };
 
-/*
 struct segment {
 	pov a, b;
 	segment() {}
 	segment(pov _1, pov _2) : a(_1), b(_2) {}
 
 	bool internal(const pov &r) {
+		return fcmp((a - r).cross(b - r)) == 0 && fcmp((a - r).dot(b - r)) < 0;
 	}
 	double distance(const pov &r) {
+		if (a == b)
+			return (r - a).len();
+		pov v1 = b - a, v2 = r - a, v3 = r - b;
+		if (fcmp(v1.dot(v2) < 0))
+			return v2.len();
+		if (0 < fcmp(v1.dot(v3)))
+			return v3.len();
+		return fabs(v1.cross(v2) / v1.len());
 	}
 	pov projection(const pov &r) {
+		pov v = b - a;
+		return a + v * (v.dot(r - a) / v.dot(v));
 	}
 	bool intersection(const segment &r) {
+		pov u = r.a - a, v = r.b - b;
+		double c1 = u.cross(b - a), c2 = u.cross(r.b - a),
+			   c3 = v.cross(a - b), c4 = v.cross(r.a - b);
+		return fcmp(c1) * fcmp(c2) < 0 && fcmp(c3) * fcmp(c4) < 0;
 	}
 };
 
-struct line {
+struct ray {
 	pov p, v;
-	line() {}
-	line(pov _1, pov _2) : p(_1), v(_2) {}
+	ray() {}
+	ray(pov _1, pov _2) : p(_1), v(_2) {}
 
 	double distance(const pov &r) {
+		random_real<double> rd;
+		rd.set(12, 345);
+		pov e = p + v * rd();
+		pov v1 = e - p, v2 = r - p;
+		return fabs(v1.cross(v2) / v1.len());
 	}
 	pov projection(const pov &r) {
+		return p + v * (v.dot(r - p) / v.dot(v));
 	}
-	pov intersection(const line &r) {
+	pov intersection(const ray &r) {
 		pov u = p - r.p;
 		double t = r.v.cross(u) / v.cross(r.p);
 		return p + v * t;
 	}
 };
-*/
---
+++ src/geometry/aoj_cgl_1_a.cpp
@@ -0,0 +1,61 @@
+#define FAST_IO 1
+#include "template.h"
+#include "basic/rand.h"
+#include "geometry/base.h"
+
+class task {
+#define ioend(cond) \
+	do {\
+		if (!(cond) || !fio.ok()) { cin.setstate(ios_base::badbit); return cin; }\
+	} while(0)
+	pov pt[2];
+	int gn;
+	vector<pov> vp, ans;
+	void preprocess() {
+		fio.set_output_float_digit(10);
+	}
+	istream &in() {
+		fio.in(pt[0].x, pt[0].y, pt[1].x, pt[1].y);
+		fio.in(gn);
+		vp.resize(gn);
+		for (auto &o : vp)
+			fio.in(o.x, o.y);
+		ioend(1);
+		return cin;
+	}
+	void deal() {
+		ans.resize(gn);
+		fup_range (i, 0, gn)
+			ans[i] = segment(pt[0], pt[1]).projection(vp[i]);
+	}
+	void out() {
+		fup_range (i, 0, ans.size())
+			fio.out(ans[i].x, ' ', ans[i].y, '\n');
+	}
+public:
+	task(
+		bool multicase = 0,
+		const char *fmt_case = 0,
+		bool blankline = 0) {
+		static int testcase = 1 << 30;
+		static stringstream tid;
+		preprocess();
+		if (multicase)
+			fio.in(testcase);
+		for (int ti = 1; ti <= testcase && in(); ++ti) {
+			deal();
+			if (blankline && 1 < ti)
+				fio.out('\n');
+			if (fmt_case)
+				fio.msg(fmt_case, ti);
+			out();
+		}
+	}
+#undef ioend
+};
+
+int main()
+{
+	new task();
+	return 0;
+}
