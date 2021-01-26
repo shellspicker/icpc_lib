c4d23685c89795d34af18596211157520e0b4c8d
shellspicker <shellspicker@outlook.com>
2021-01-26 16:32:22 +0800

    update include/graph/topo.h
    test by cf 1385e.
    
    Signed-off-by: shellspicker <shellspicker@outlook.com>

--- include/graph/topo.h
+++ include/graph/topo.h
@@ -1,76 +1,95 @@
-#define NODE_C typename graph::node
-#define EDGE_C typename graph::edge
-
-template<typename graph>
-struct topo {
-	using node = NODE_C;
-	using edge = EDGE_C;
-
-	graph *g;
-	node *nd;
-
-	topo(graph *g_) : g(g_), nd(&g->nodes[0]) {}
+/*
+ * bfs
+ *
+ * macro:
+ *   BFS
+ *
+ * node.meta:
+ *   int in(0), pos;
+ */
 #if BFS
-	pair<bool, vector<int>> sort_bfs() {
+template<typename graph>
+class topo_bfs {
+	using node = typename graph::node_t;
+	using edge = typename graph::edge_t;
+	graph &g;
+	node *nd;
+public:
+	topo_bfs(graph &_1) : g(_1), nd(g.node_base) {}
+	pair<bool, vector<int>> get() {
 #define add(o) \
 		if (!o.meta.in) ({ \
 			que.push(o.id()); \
 		})
-
 		node *v;
 		edge *e;
 		vector<int> ret;
 		queue<int> que;
-		fup_range (u, 0, g->size())
-			add(u[nd]);
+		fup_range (u, 0, g.size())
+			add(g[u]);
 		while (!que.empty()) {
 			int u = que.front();
 			que.pop();
-			u[nd].meta.pos = ret.size();
+			g[u].meta.pos = ret.size();
 			ret.push_back(u);
-			for (int ei : u[nd].link) {
-				tie(v, e) = g->extend(ei);
+			for (int ei : g[u].link) {
+				tie(v, e) = g.extend(ei);
 				v->meta.in--;
-				add(v->id()[nd]);
+				add(g[v->id()]);
 			}
 		}
-		return {ret.size() == g->size(), ret};
+		return {ret.size() == g.size(), ret};
 #undef add
-	}
-#endif
-
-#if DFS
-	pair<bool, vector<int>> sort_dfs() {
-		vector<int> ret;
-		function<bool(node *)> dfs = [&](node *u) -> bool {
-			node *v;
-			edge *e;
-			auto &mt = u->meta;
-			mt.flag = -1;
-			for (int ei : u->link) {
-				tie(v, e) = g->extend(ei);
-				if (!~v->meta.flag)
-					return 0;
-				if (!v->meta.flag && !dfs(v))
-					return 0;
-			}
-			mt.flag = 1;
-			ret.push_back(u->id());
-			return 1;
-		};
-		fup_range (i, 0, g->size())
-			if (!i[nd].meta.flag)
-				dfs(&i[nd]);
-		reverse(it_each(ret));
-		fup_range (i, 0, ret.size())
-			ret[i][nd].meta.pos = i;
-		return {ret.size() == g->size(), ret};
 	}
 };
 #endif
 
-#undef NODE_C
-#undef EDGE_C
+/*
+ * dfs
+ *
+ * macro:
+ *   DFS
+ *
+ * node.meta:
+ *   int flag(0), pos;
+ */
+#if DFS
+template<typename graph>
+class topo_dfs {
+	using node = typename graph::node_t;
+	using edge = typename graph::edge_t;
+	graph &g;
+	node *nd;
+public:
+	topo_dfs(graph &_1) : g(_1), nd(g.node_base) {}
+	pair<bool, vector<int>> get() {
+		vector<int> ret;
+		function<bool(int)> dfs = [&](int u) -> bool {
+			node *v;
+			edge *e;
+			auto &mt = g[u].meta;
+			mt.flag = -1;
+			for (int ei : g[u].link) {
+				tie(v, e) = g.extend(ei);
+				if (!~v->meta.flag)
+					return 0;
+				if (!v->meta.flag && !dfs(v->id()))
+					return 0;
+			}
+			mt.flag = 1;
+			ret.push_back(u);
+			return 1;
+		};
+		fup_range (u, 0, g.size())
+			if (!g[u].meta.flag)
+				dfs(u);
+		reverse(it_each(ret));
+		fup_range (i, 0, ret.size())
+			g[ret[i]].meta.pos = i;
+		return {ret.size() == g.size(), ret};
+	}
+};
+#endif
 
 #undef BFS
 #undef DFS
---
+++ src/graph/topo/cf_1385e_dfs.cpp
@@ -0,0 +1,87 @@
+#define FAST_IO 1
+#include "template.h"
+#include "graph/graph.h"
+#define DFS 1
+#include "graph/topo.h"
+
+struct nif {
+	int flag, pos;
+	nif() {
+		flag = 0;
+	}
+};
+using graph_t = graph<graph_node<nif>, graph_edge<fake_type>>;
+
+class task {
+#define ioend(cond) \
+	do {\
+		if (!(cond) || !fio.ok()) { cin.setstate(ios_base::badbit); return cin; }\
+	} while(0)
+	graph_t grp;
+	bool ok;
+	void preprocess() {
+		fio.set_output_float_digit(12);
+	}
+	istream &in() {
+		int n, m;
+		fio.in(n, m);
+		grp.resize(n);
+		fup_range (t, 0, m) {
+			int d, a, b;
+			fio.in(d, a, b);
+			a--, b--;
+			if (d) {
+				grp.add(a, b);
+			} else {
+				grp.add_edge_only(a, b);
+			}
+		}
+		ioend(1);
+		return cin;
+	}
+	void deal() {
+		topo_dfs<graph_t> tp_sort(grp);
+		tie(ok, ignore) = tp_sort.get();
+	}
+	void out() {
+		if (ok) {
+			fio.msg("YES\n");
+			fup_range (i, 0, grp.edges.size()) {
+				auto &e = grp.edges[i];
+				auto &from = grp[e.from].meta;
+				auto &to = grp[e.to].meta;
+				if (from.pos < to.pos)
+					fio.msg("%d %d\n", e.from + 1, e.to + 1);
+				else
+					fio.msg("%d %d\n", e.to + 1, e.from + 1);
+			}
+		} else {
+			fio.msg("NO\n");
+		}
+	}
+public:
+	task(
+		bool multicase = 0,
+		const char *fmt_case = 0,
+		bool blankline = 0) {
+		static int testcase = 1 << 30;
+		preprocess();
+		if (multicase)
+			fio.in(testcase);
+		for (int ti = 1; ti <= testcase && in(); ++ti) {
+			deal();
+			if (blankline && 1 < ti)
+				fio.out('\n');
+			if (fmt_case)
+				fio.msg(fmt_case, ti);
+			out();
+		}
+	}
+#undef ioend
+};
+
+int main()
+{
+	new task(1);
+	return 0;
+}
