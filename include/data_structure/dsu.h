1fdcbb87352c02bc1b18fb0e8a52752dec7d6382
shellspicker <shellspicker@outlook.com>
2021-07-26 22:52:32 +0800

    dsu: get group id
    Signed-off-by: shellspicker <shellspicker@outlook.com>

--- include/data_structure/dsu.h
+++ include/data_structure/dsu.h
@@ -16,15 +16,25 @@ public:
 	int set(int x) {
 		return fa[x] == x ? x : fa[x] = set(fa[x]);
 	}
-	bool link(int master, int slave) {
-		int x = set(master), y = set(slave);
-		if (x == y)
+	template<typename ...var>
+	void id(var &&...args) {
+		initializer_list<int>{(args = set(args), 0)...};
+	}
+	bool link(int mas, int slv) {
+		id(mas, slv);
+		if (mas == slv)
 			return 0;
-		fa[y] = x;
+		fa[slv] = mas;
 		return 1;
 	}
 	bool same(int x, int y) {
-		return set(x) == set(y);
+		id(x, y);
+		return x == y;
 	}
 };
+
+template<class ds, class grp>
+void comb_helper(ds &ref, grp &cc, typename ds::node_t *o, int a, int b) {
+	ref[a] == o ? cc.link(a, b) : cc.link(b, a);
+}
 
